58d5b86dd00f148c2497d38e82ec8f1e043330ce
shimabox <shimabox@gmail.com>
2024-12-15 23:01:26 +0900

    feat: Guzzle サンプルその2(同期リクエスト)

--- api/app/routes/web.php
+++ api/app/routes/web.php
@@ -12,3 +12,11 @@ Route::get('/sample_1/{id}', function ($id) {
         'id' => $id,
     ]);
 });
+
+// Guzzle サンプルその2(同期リクエスト)
+Route::get('/sample_2/{id}', function ($id) {
+    sleep(1);
+    return response()->json([
+        'id' => $id,
+    ]);
+});
---
+++ client/sample/sample_2.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Sample;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request;
+
+$baseUrl = $_ENV['API_BASE_URL'] ?? 'http://api';
+
+$client = new Client([
+    'base_uri' => $baseUrl,
+]);
+
+$start  = hrtime(true);
+
+$client->sendRequest(new Request('GET', '/sample_2/1'));
+$client->sendRequest(new Request('GET', '/sample_2/12'));
+$client->sendRequest(new Request('GET', '/sample_2/123'));
+
+$total = (hrtime(true) - $start) / 1e+9;
+echo "{$total} 秒" . PHP_EOL;
