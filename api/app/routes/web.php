297dc594fa7df58cfdf0b8c1a56673c83fbf5709
shimabox <shimabox@gmail.com>
2024-12-16 01:11:41 +0900

    feat: 型付けとく

--- api/app/routes/web.php
+++ api/app/routes/web.php
@@ -7,14 +7,14 @@ Route::get('/', function () {
 });
 
 // Guzzle サンプルその1(同期リクエスト)
-Route::get('/sample_1/{id}', function ($id) {
+Route::get('/sample_1/{id}', function (int $id) {
     return response()->json([
         'id' => $id,
     ]);
 });
 
 // Guzzle サンプルその2(同期リクエスト)
-Route::get('/sample_2/{id}', function ($id) {
+Route::get('/sample_2/{id}', function (int $id) {
     sleep(1);
     return response()->json([
         'id' => $id,
@@ -22,7 +22,7 @@ Route::get('/sample_2/{id}', function ($id) {
 });
 
 // Guzzle サンプルその3(非同期リクエスト)
-Route::get('/sample_3/{id}', function ($id) {
+Route::get('/sample_3/{id}', function (int $id) {
     sleep(1);
     return response()->json([
         'id' => $id,
@@ -30,7 +30,7 @@ Route::get('/sample_3/{id}', function ($id) {
 });
 
 // Guzzle サンプルその4(非同期リクエスト Pool)
-Route::get('/sample_4/{id}', function ($id) {
+Route::get('/sample_4/{id}', function (int $id) {
     return response()->json([
         'id' => $id,
     ]);
